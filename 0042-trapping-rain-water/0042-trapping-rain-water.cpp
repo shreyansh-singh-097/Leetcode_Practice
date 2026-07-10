c8ba764f5b20384cdf910db9b9c8986670866a11
Shreyansh Singh <shreyansh.singh.r06@gmail.com>
2026-07-10 08:52:49 +0530

    Time: 2 ms (19.85%), Space: 26.1 MB (57.71%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -2,24 +2,26 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size();
-        vector<int> lMax(n,0);
-        vector<int> rmax(n,0);
-        lMax[0] = height[0];
-        rmax[n-1] = height[n-1];
-        for (int i = 1; i < n; i++) {
-            lMax[i] = max(lMax[i - 1], height[i]);
-        }
-        for (int i = n - 2;i >= 0; i--) {
-            rmax[i] = max(rmax[i + 1], height[i]);
-        }
+        int left = 0;
+        int right = n - 1;
+        int leftMax = 0;
+        int rightMax = 0;
         int ans = 0;
-        // Calculate trapped water
-        for (int i = 0; i < n; i++) {
-            ans += min(lMax[i], rmax[i]) - height[i];
+        while (left <= right) {
+            if (height[left] <= height[right]) {
+                if (height[left] >= leftMax)
+                leftMax = height[left];
+                else
+                ans += leftMax - height[left];  left++;
+            }
+            else {
+                if (height[right] >= rightMax)
+                    rightMax = height[right];
+                else ans += rightMax - height[right];  right--;
+            }
         }
         return ans;
     }
 };
 
-
-// prefix array approach O(n) SC & TC
+// Two pointer approach 
